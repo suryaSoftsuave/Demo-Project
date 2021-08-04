1b9c46470ea4691e1495ab8e5cc67391bab798a6
suryaSoftsuave <surya.kandhasamy@softsuave.com>
2021-08-04 18:19:27 +0530

    second change removed

--- src/main/java/com/todo/TodoController.java
+++ src/main/java/com/todo/TodoController.java
@@ -46,7 +46,6 @@ public class TodoController {
 		}
 		else {
 			todoService.saveTodo(todo);
-			System.out.println("Todo successfully added");
 			mv.addObject("listOfTodos", todoService.getAllTodos());
 			mv.setViewName("listTodo");
 		}
